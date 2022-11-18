76cd84c80942266138609dbb893859c033d9ae59
Jorge Apaza <jorgeapaza2124@gmail.com>
2022-11-17 23:48:18 -0300

    desafio de chat

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//IMPLEMENTACION
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer);
+
+httpServer.listen(PORT, () =>
+  console.log("SERVER ON http://localhost:" + PORT)
+);
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+let msgs = [];
+//leer el archivo y cargar los msgs
+
+io.on("connection", (socket) => {
+  //console.log("se creo un socket nuevo " + socket.id);
+  // socket.emit("msg", "hola front!");
+  msgs.push({
+    socketid: socket.id,
+    email: "",
+    mensaje: " se conecto fulanito " + socket.id,
+  });
+  io.sockets.emit("msg-list", msgs);
+
+  socket.on("msg", (data) => {
+    console.log("data", data);
+    msgs.push({
+      socketid: socket.id,
+      email: data.email,
+      mensaje: data.mensaje,
+      //gregar el campo fecha
+    });
+    //persistir en un archivo
+
+    io.sockets.emit("msg-list", msgs);
+  });
+  /* socket.on("disconnect", (reason) => {
+    console.log(reason + " " + socket.id);
+  }); */
+});
